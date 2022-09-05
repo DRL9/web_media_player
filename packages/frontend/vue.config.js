05bc2ae749197f1b55df9a6b11c6d87aa2ba38fd
Yuanjun Zhong <937676516@qq.com>
2022-09-05 09:13:14 +0800

    update pkg

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -3,7 +3,7 @@ const localhost = (() => {
   const nets = require('os').networkInterfaces();
   return Object.values(nets)
     .reduce((pre, cur) => cur.concat(pre))
-    .find(a => /192\.168/.test(a.address)).address;
+    .find((a) => /192\.168/.test(a.address)).address;
 })();
 console.log(localhost);
 process.env.VUE_APP_HOST = localhost;
